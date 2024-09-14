d8017a88854735dd4e77d2f15a61963bb08d2e6c
Treyson Le <treysonle11@gmail.com>
2024-09-14 17:35:20 -0600

    Added section for new features

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+// Make array of roll history so it can be viewed again
+// Add 10 roll function
 public class Main {
 
-
-    // NEED TO ADD GUARANTEED 4 STAR FEATURE
     public static int rollResult (int rollHistory) {
         int rollResult = 0;
 
@@ -19,9 +20,11 @@ public class Main {
             System.out.println("    Error, reset Roll Counter");
             rollResult = 3;
         }
+        // Hard Pity
         if (rollHistory == 90) {
             rollResult = 2;
         }
+        // Soft Pity
         if (rollHistory > 75 && rollHistory < 90) {
             if (rollRng < 325) {
                 rollResult = 2;
@@ -40,47 +43,77 @@ public class Main {
         }
         return rollResult;
     }
+
+    //public static int runRoll {
+
+    //}
     public static void main(String[] args) {
-        int val = 0;
-        int rollHistory = 0;
-        int reward = 0;
         Scanner scr = new Scanner(System.in);
+        int val = 0,
+            rollHistory = 0,
+            midHistory = 0,
+            reward;
+
 
         System.out.println("\n     Welcome to 50/50 Cope");
         System.out.println("     ---------------------");
-        System.out.println("     Enter (1) to wish");
-        System.out.println("     Enter (2) to stop");
+        System.out.println("     Enter (1) to Single wish");
+        System.out.println("     Enter (2) to Multi Wish (10)");
+        System.out.println("     Enter (3) to Check Wish History");
+        System.out.println("     Enter (4) to Stop");
 
-        while (val != 2) {
+        while (val != 4) {
             System.out.print("     Enter Choice: ");
             val = scr.nextInt();
 
-            if (val == 1) {
-                System.out.println("    Preparing to Roll...");
-                rollHistory++;
-                reward = rollResult(rollHistory);
+            switch(val) {
+                case 1:
+                    System.out.println("\n    Preparing to Roll...");
+                    rollHistory++;
 
-                switch(reward) {
-                    case 0:
-                        System.out.println();
-                        System.out.println("    3 star haha get fucked");
-                        break;
-                    case 1:
-                        System.out.println();
-                        System.out.println("    4 star woah good job");
-                        // Need to reset 4 star pity
-                        break;
-                    case 2:
-                        System.out.println();
-                        System.out.println("    5 star yippie ٩(ˊᗜˋ*)و ♡");
-                        // Need to reset 5 star pity
-                        break;
-                }
-            }
-            else {
-                System.out.println();
-                System.out.println("    Quitting...");
+                    reward = rollResult(rollHistory);
+
+                    // Guaranteed 4 star
+                    if (midHistory >= 9 && reward != 2) {
+                        reward = 1;
+                    }
+                    switch(reward) {
+                        case 0:
+                            System.out.println();
+                            System.out.println("    3 star haha get fucked\n");
+                            midHistory++;
+                            break;
+                        case 1:
+                            System.out.println();
+                            System.out.println("    4 star woah good job\n");
+                            midHistory = 0;
+                            break;
+                        case 2:
+                            System.out.println();
+                            System.out.println("    5 star yippie ٩(ˊᗜˋ*)و ♡\n");
+                            // Resets Pity
+                            // Add Method to determine 50/50
+                            midHistory++;
+                            rollHistory = 0;
+                            break;
+                    }
+                    break;
+                case 2:
+                    // ADD 10 ROLL
+                    System.out.println("Rolling 10 times");
+                    break;
+                case 3:
+                    // ADD ROLL HISTORY
+                    System.out.println("Checking roll history");
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Enter valid choice");
+                    break;
             }
         }
+        System.out.println();
+        System.out.println("    Quitting...");
     }
 }
